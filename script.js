1743aad05d9fd150a9f8430f5d54cb99c77d5bf7
sidane17 <khalilsidane89@gmail.com>
2024-04-16 17:18:12 +0200

    js

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ document.getElementById("submit").onclick = function(){
     let a = document.getElementById("nome").value;
     let b = document.getElementById("cognome").value;
     let c  = document.getElementById("colore").value;
-    console.log(a);
-    console.log(b);
-    console.log(c);
+  
     let password = a+b+c+z;
     document.getElementById("cavallo").innerHTML=password;
 
